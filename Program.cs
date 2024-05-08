b88bc7742a3c00e1bd838fac682157d10892f949
DebDAB <prrrr116@gmail.com>
2024-05-08 09:49:58 +0400

    Initial

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A:");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("B:");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Операция: \n1. '+'\n2.'-'\n3.'*'\n4.'/'\n5.Выход");
+
+
+            int Op = int.Parse(Console.ReadLine());
+            int Rez;
+            while (Op != 5)
+            {
+                if (Op == 1)
+                {
+                    Rez = A + B;
+                    Console.WriteLine(Rez);
+                }
+                if (Op == 2)
+                {
+                    Rez = A - B;
+                    Console.WriteLine(Rez);
+                }
+                if (Op == 1)
+                {
+                    Rez = A * B;
+                    Console.WriteLine(Rez);
+                }
+                if (Op == 1)
+                {
+                    Rez = A / B;
+                    Console.WriteLine(Rez);
+                }
+            }
+
+
+        }
+    }
+}
